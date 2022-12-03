c5110e99a31921206c8a73e0e2f00f53682557fe
David Ham <davidham@davids-mbp.lan>
2022-12-03 12:10:41 -0500

    comitting

--- R/main_funcs.R
+++ R/main_funcs.R
@@ -16,6 +16,7 @@ get_eta = function(t_star, alpha) {
 #'
 #' @param data A dataframe containing treatment and response (W, Y)
 #' @param treatment Character string specifying the column name of data that contains treatment
+#' Assume treatment to be numeric. E.g. (0,1) for binary.
 #' @param response Character string specifying the column name of data that contains response
 #' @param PS Optional vector of length nrow(data) that specifies the propensity scores.
 #' If left blank, it is assumed to 0.5 for all units in the data.
@@ -27,6 +28,9 @@ get_eta = function(t_star, alpha) {
 #' @param nonasymp Boolean that specifies to return an asymptotic confidence sequence (FALSE)
 #' or a non-asymptotic confidence sequence (TRUE). Default is FALSE.
 #' @param M only supplied if nonasymp is TRUE. Hyperparameter corresponding to maximum response.
+#' @param comparison Optional vector of length 2 used for treatments with more than 2 groups.
+#' Example: comparison = c(0,1) obtains ATE of Y(1) - Y(0) and comparison = c(0, 2)
+#' obtains ATE of Y(2) - Y(0). Comparison must match the same value of supplied treatment
 #'
 #' @return A list containing: \item{lower}{A vector containing the lower confidence sequence.}
 #' \item{upper}{A vector containing the upper confidence sequence.}
@@ -38,31 +42,36 @@ get_eta = function(t_star, alpha) {
 #' @references Ham, D., Bojinov, I., Lindon, M., Tingley, M. (2022)
 #' Design-Based Confidence Sequences for Anytime-valid Causal Inference
 DB_CS = function(data, treatment, response, PS = rep(0.5, nrow(data)), alpha = 0.05,
-                 t_opt = 10, proxy_outcomes = NULL, nonasymp = FALSE, M = NULL) {
+                 t_opt = 10, proxy_outcomes = NULL, nonasymp = FALSE, M = NULL, comparison = c(0, 1)) {
   # checks
   if (!(treatment %in% colnames(data))) stop("treatment should be in data")
+  if (!(class(data[, treatment]) == "numeric")) stop("treatment should be numeric")
   if (!(response %in% colnames(data))) stop("response should be in data")
   if (nonasymp & is.null(M)) stop("M is required if nonasymp is TRUE")
+  if (!(all(comparison %in%  unique(data[, treatment])))) stop("treatment does not match comparison")
 
   # setup
   y = data[, response]
   W = data[, treatment]
   t = nrow(data)
-
+  in_idx = as.numeric(W %in% comparison)
+  signW = W
+  signW[signW == comparison[1]] = 0
+  signW[signW == comparison[2]] = 1
   # main proposed asymptotic CS
   if (nonasymp == FALSE) {
     eta = get_eta(t_star = t_opt, alpha = alpha)
     if (is.null(proxy_outcomes)) {
       # without proxy outcomes
-      tau_hat = y/PS*(-1)^(1-W)
-      sig2_hat = y^2/PS^2
+      tau_hat = y/PS*(-1)^(1-signW)*in_idx
+      sig2_hat = y^2/PS^2*in_idx
 
     } else {
       # with proxy outcomes
       res_y = y - proxy_outcomes
 
-      tau_hat = res_y/PS*(-1)^(1-W)
-      sig2_hat = res_y^2/PS^2
+      tau_hat = res_y/PS*(-1)^(1-signW)*in_idx
+      sig2_hat = res_y^2/PS^2*in_idx
 
     }
 
@@ -74,8 +83,8 @@ DB_CS = function(data, treatment, response, PS = rep(0.5, nrow(data)), alpha = 0
 
   } else{
     p_min = min(PS)
-    tau_hat = y/PS*(-1)^(1-W)
-    sig2_hat = y^2/PS^2
+    tau_hat = y/PS*(-1)^(1-signW)*in_idx
+    sig2_hat = y^2/PS^2*in_idx
     Sn = cumsum(sig2_hat)
     m = M/p_min
     width = 1/(1:t)*(m*(m+1)*log(2/alpha) + Sn*((m+1)/m *log(1 + 1/m) - 1/m))
